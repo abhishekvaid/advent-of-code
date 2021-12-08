7a9c612e379882dfaf86b69dc69ff27a568f3a63
Abhishek Vaid <vaid.abhi@gmail.com>
2021-12-08 16:17:51 +0530

    Day 5 in progress

--- day-1/solution.py
+++ day-1/solution.py
@@ -1,13 +1,15 @@
-entries = list(map(int, open("input.txt").read().split("\n")))
+def input():
+    return list(map(int, open("input.txt").read().split("\n")))
 
 
 def part1():
+    entries = input()
     print("part1: ", sum(1 if y > x else 0 for x, y in zip(entries[:-1], entries[1:])))
 
 
 def part2():
+    entries = input()
     print("part2: ", sum(1 if y > x else 0 for x, y in zip(entries[:-1], entries[3:])))
-
 
 
 if __name__ == "__main__":
